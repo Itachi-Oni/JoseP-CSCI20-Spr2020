67a940a9eab221526ac2a71114bea53483b82e79
Itachi-Oni <josepadilla2001.jp@gmail.com>
2020-02-12 03:32:48 +0000

    Lab 1.4.2

--- main.go
+++ main.go
@@ -1,47 +1,53 @@
-// Programmer name: Jose Padilla
-// Date completed:  2/6/2020
-// Description: You Code It- Week 2
-
+//Created by: Jose Padilla
+//Created on: Some day
+//Description: This program runs the code for a lab game called the 99 trick.
+  
 package main
 
-import "fmt" 
+import "fmt"
 
 func main() {
-  favFood := "Brownies"
-  var name string
-  var age string
-    //output a welcome statement using their name
-    //output a statement that says At their age you enjoyed the favorite food
- 
-  fmt.Println("Please enter your name: ")
- 
-  fmt.Scanln(&name)
+
+  var noun0 string
+  fmt.Print("Enter a noun: ")
+  fmt.Scan(&noun0)
+  fmt.Println(noun0)
   
+   var adjective0 string
+  fmt.Print("Enter an adejective: ")
+  fmt.Scan(&adjective0)
+  fmt.Println(adjective0)
 
-  fmt.Println("Please enter your age: ")
- 
-  fmt.Scanln(&age)
-  
-  fmt.Println("Welcome", name, ".")
-  fmt.Println("At age",age,",",name, "enjoyed eating", favFood,".")
+   var noun1 string
+  fmt.Print("Enter a noun: ")
+  fmt.Scan(&noun1)
+  fmt.Println(noun1)
 
-  
+   var location0 string
+  fmt.Print("Enter a location: ")
+  fmt.Scan(&location0)
+  fmt.Println(location0)
 
- 
+   var adjective1 string
+  fmt.Print("Enter an adjective: ")
+  fmt.Scan(&adjective1)
+  fmt.Println(adjective1)
 
- 
+  var noun2 string
+  fmt.Print("Enter a noun: ")
+  fmt.Scan(&noun2)
+  fmt.Println(noun2)
 
+  fmt.Print("There once was a ", noun0, " who had a ", adjective0," pet ", noun1,". ", "They went on an adventure to ", location0, ", to recover the ", adjective1," ", noun2,"." )
 
+  var num int
+  fmt.Print("Enter a number: ")
+  fmt.Scan(&num)
+  fmt.Println(num)
 
-
-
-
-
-
-
-
-
-
-
+  var ber int
+  fmt.Print("Enter another number: ")
+  fmt.Scan(&ber)
+  fmt.Println(ber)
+  fmt.Print(num, "+", ber, "=",num + ber, num, "-", ber, "=",num - ber, num, "*", ber, "=", num * ber, num, "/", ber, "=", num / ber)
 }
-
