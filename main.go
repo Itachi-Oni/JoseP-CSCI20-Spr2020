de70aca050c762514b72dc331e616cae13d639ac
Itachi-Oni <josepadilla2001.jp@gmail.com>
2020-02-14 02:29:08 +0000

    Project 1 w/Comments

--- main.go
+++ main.go
@@ -5,11 +5,16 @@
 package main
 import "fmt"
 func main() {
+  //Sets user input variable
   var far float64
   fmt.Println("This program is used to convert Farenheit into Celsius and Kelvin.")
   fmt.Print("Please enter a temperature in Farenheit.")
+  //Scans for user input to assign to variable
   fmt.Scan( &far)
+  //Converts Farenheit to Kelvin
   kel := (far-32) * 5/9 + 273.15
+  //Converts Farenheit to Celsius
   cel := (far - 32) * 5/9
+  //Prints the results
   fmt.Print("Farenheit: ", far," Kelvin: ", kel, " Celsius: ", cel)
 }
