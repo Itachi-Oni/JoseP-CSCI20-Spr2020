8ffa6d3dfda8fd85ae8118fe749d582fc329bed1
Itachi-Oni <josepadilla2001.jp@gmail.com>
2020-02-28 01:56:16 +0000

    2.3.1 Lab Part 1

--- main.go
+++ main.go
@@ -1,41 +1,46 @@
 // Programmer name: Jose Padilla
-// Date completed:  2/20/2020
-// Description: Project 1: A temperature converter.
+// Date completed:  2/27/2020
+// Description: Rock Paper Scissors game-p1 vs cpu
 
 package main
-import "fmt"
+
+import (
+    "fmt"
+    "math/rand"
+    "time"
+) //adding the ability to do random numbers
+
 func main() {
- var x [4]int
- var user0 int
- var user1 int
- var user2 int
- var user3 int
 
-  fmt.Println("Please enter a single digit number. 1/4 ")
-  fmt.Scanln(&user0)
-  fmt.Println(user0)
-  x[0] = user0
+  var user int 
+  var cpu int
+    //create two variables - one for the computer and one for the user
 
-  fmt.Println("Please enter a single digit number. 2/4 ")
-  fmt.Scanln(&user1)
-  fmt.Println(user1)
-  x[1] = user1
+  s1 := rand.NewSource(time.Now().UnixNano())
+  r1 := rand.New(s1)
 
-    fmt.Println("Please enter a single digit number. 3/4 ")
-  fmt.Scanln(&user2)
-  fmt.Println(user2)
-  x[2] = user2
+  cpu = r1.Intn(3)
 
-    fmt.Println("Please enter a single digit number. 4/4 ")
-  fmt.Scanln(&user3)
-  fmt.Println(user3)
-  x[3] = user3
+    //use a random integer value representing the computer's choice in a game of Rock, Scissors, Paper. 0=rock, 1=scissors, 2=paper
 
-  fmt.Println(x)
-  added := user0 + user1 + user2 + user3
-  fmt.Println(added)
-for added <= 9 && added >= 1{
-  added = user0 + user1 + user2 + user3
-  fmt.Println(added)
+  fmt.Println("Please enter 0 for rock, 1 for scissors or 2 for paper")
+  fmt.Scanln(&user)
+    //prompt the user for an integer value representing the player's choice
+  if cpu == 0 {
+    fmt.Println("Computer chose rock")
+  } else if cpu == 1 {
+    fmt.Println("Computer chose scissors")
+  }else if cpu == 2 {
+    fmt.Println("Computer chose papers")
   }
-} 
+  if user == 0 {
+    fmt.Println("you chose rock")
+  } else if user == 1 {
+    fmt.Println("you chose scissors")
+  }else if user == 2 {
+    fmt.Println("you chose papers")
+  }
+  
+    //Print out the values using the words rock, scissors, paper.  ie. "Computer chose rock and player chose paper"
+    //You will need to use decisions for this
+}
